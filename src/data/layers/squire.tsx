cadde4d72aaeb8e2bfb04980d664fc336e73a779
Nathan <nathan.public@gmail.com>
2023-05-08 20:48:42 -0400

    learning prospectus

--- src/data/layers/squire.tsx
+++ src/data/layers/squire.tsx
@@ -2,7 +2,7 @@
  * @module
  * @hidden
  */
-import { farmer } from "./farmer";
+import farmer from "./farmer";
 import { createCumulativeConversion } from "features/conversion";
 import { jsx } from "features/feature";
 import { createHotkey } from "features/hotkey";
